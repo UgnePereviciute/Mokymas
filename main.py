b6f020dd263bef102dd2103be51e2bc3a102a8a5
Ugne Pereviciute <u.pereviciute@gmail.com>
2023-02-01 21:30:46 +0200

    pridetas antras failas

---
+++ main.py
@@ -0,0 +1,9 @@
+from modules.python_kursas import PythonKursas, Kursas
+
+
+paskaita = PythonKursas("Python", "Tomas", 4)
+paskaita2 = Kursas("Java", "Lukas", 4)
+
+paskaita.destyti()
+paskaita2.destyti()
+
---
+++ modules/python_kursas.py
@@ -0,0 +1,10 @@
+from modules.kursas import Kursas
+
+
+class PythonKursas(Kursas):
+    def __init__(self, pavadinimas, destytojas, trukme):
+       super().__init__(pavadinimas, destytojas, trukme)
+
+    def destyti(Kursas):
+        print("Vyksta programavimas")
+
